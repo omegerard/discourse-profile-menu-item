18d57d9ce5670f8d8fee4a0364f56b7bec1e7bfa
Ludo Vangilbergen <ludo@vangilbergen.net>
2024-12-14 17:35:54 +0100

    javascript aangepast

--- javascripts/discourse/initializers/dynamic-profile-link.js
+++ javascripts/discourse/initializers/dynamic-profile-link.js
@@ -1,1 +1,14 @@
-console.log("My GiPSo Discourse Theme Component werkt!");
+export default {
+  name: "dynamic-profile-link", // Naam van de initializer (vrij te kiezen)
+
+  initialize() {
+    console.log("My GiPSo Discourse Theme Component werkt!");
+
+    // Voeg hier je functionaliteit toe
+    const currentUser = Discourse.User.current();
+    if (currentUser) {
+      console.log(`Gebruiker ingelogd: ${currentUser.username}`);
+    }
+  }
+};
+
