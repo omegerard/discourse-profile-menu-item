289bb921f936473163a2c3627606aa5d520a3825
Ludo Vangilbergen <ludo@vangilbergen.net>
2024-12-14 14:44:25 +0100

    Added code for debugging

--- common/javascripts/initializers/dynamic-profile-link.js
+++ common/javascripts/initializers/dynamic-profile-link.js
@@ -4,19 +4,25 @@ require(["application", "I18n"], function (Discourse, I18n) {
       this._super(...arguments);
 
       const currentUser = Discourse.User.current();
+      console.log("Current User:", currentUser);  // Log de huidige gebruiker
+
       if (currentUser) {
         const username = currentUser.username;
+	console.log("Username:", username);  // Log de gebruikersnaam
 
         if (!document.querySelector('li[data-list-item-name="profile-link"]')) {
           const menuList = document.querySelector("#sidebar-section-content-community");
+	  console.log("Menu List Found:", menuList);  // Log het menu-element
 
           if (menuList) {
             const listItem = document.createElement("li");
             listItem.setAttribute("data-list-item-name", "profile-link");
             listItem.className = "sidebar-section-link-wrapper";
+	    console.log("List Item Created:", listItem);  // Log het gemaakte item
 
             // Gebruik de vertaling
             const menuName = I18n.t("js.menu_name");
+            console.log("Menu Name:", menuName);  // Log de vertaling
 
             listItem.innerHTML = `
               <a class="ember-view sidebar-section-link sidebar-row" 
@@ -34,6 +40,7 @@ require(["application", "I18n"], function (Discourse, I18n) {
             `;
 
             menuList.insertBefore(listItem, menuList.firstChild);
+	    console.log("List Item Inserted:", listItem);  // Log het ingevoegde item
           }
         }
       }
